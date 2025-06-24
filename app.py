487d5513d61988f0854a1f3fc813b404f5a80d28
ShubbhamJain <shubham.jain368@gmail.com>
2025-06-25 02:28:54 +0530

    add: log

--- app.py
+++ app.py
@@ -3,5 +3,6 @@ import json
 
 def lambda_handler(event, context):
     print(json.dumps(event))
+    print(json.dumps(context))
     print("Hello from Lambda via docker and ECR!")
     return {"statusCode": 200, "body": json.dumps("Hello from Lambda!")}
